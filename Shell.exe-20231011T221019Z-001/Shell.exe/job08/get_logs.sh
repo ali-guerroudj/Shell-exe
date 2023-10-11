b09face87eafc1515835979df822861fb34e91d2
ali-guerroudj <146730586+ali-guerroudj@users.noreply.github.com>
2023-10-12 00:12:15 +0200

    Add files via upload

---
+++ Shell.exe-20231011T221019Z-001/Shell.exe/job08/get_logs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cat /var/log/./auth.log > /home/ali/Shell.exe/job08/./number_connection-10-10-2023-00:16 
+
+tar -cvf /home/ali/Shell.exe/job08/Backup/./number_connection-10-10-2023-00:16.tar number_connection-10-10-2023-00:16 
+
+echo number_connection-10-10-2023-00:16.tar number_connection-10-10-2023-00:16 Backup 
+
